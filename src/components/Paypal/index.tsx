0a88bc7c5cf177d78f76e76f695045183e5f4181
Diego Rodriguez Paiva <diegorodriguez93@hotmail.com>
2023-08-18 19:08:24 -0300

    last changes in the FE for the deploy

---
+++ src/components/Paypal/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+import { PayPalButton } from "react-paypal-button-v2";
+
+const PaypalButtonDiv: React.FC = () => {
+  const CLIENT_ID_NOSUB =
+    "ATWdOnaxQKdaZsz1fyXHyATWO2b8sI5qG_t8ukWRSG6zLQlOOGIArCGnA5F-1dV4BPiMme3_RvEyFQjI&currency=USD";
+
+  return (
+    <div className="input-group cien">
+      <div>
+        <PayPalButton
+          amount={35}
+          // shippingPreference="NO_SHIPPING" // default is "GET_FROM_FILE"
+          onSuccess={() => {
+            /*             Swal.fire({
+              title: "Correcto!",
+              text:
+                "Hemos procesado el pago correctamente, no olvides darle al boton registrarme",
+              icon: "success",
+            }); */
+
+            // localStorage.setItem("payment_approved", 1);
+            console.log("a");
+          }}
+          options={{
+            clientId: CLIENT_ID_NOSUB,
+          }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default PaypalButtonDiv;
--- src/pages/Reservas.tsx
+++ src/pages/Reservas.tsx
@@ -1,16 +1,23 @@
-import { useState } from 'react';
-import { DateRangePicker, StaticRange } from 'react-date-range';
-import 'react-date-range/dist/styles.css';
-import 'react-date-range/dist/theme/default.css';
-import { es } from 'date-fns/locale';
-import { addDays } from 'date-fns';
+import { useState } from "react";
+import { DateRangePicker, StaticRange } from "react-date-range";
+import "react-date-range/dist/styles.css";
+import "react-date-range/dist/theme/default.css";
+import { es } from "date-fns/locale";
+import { addDays } from "date-fns";
 
-import { Layout } from '../components/Layout';
+import { Layout } from "../components/Layout";
+import "./reservas.css";
+import { Button, Col, Row } from "react-bootstrap";
+import PaypalButtonDiv from "../components/Paypal";
 
 type Range = {
-  startDate?: Date;
-  endDate?: Date;
-  key?: string;
+  startDate?: Date | undefined;
+  endDate?: Date | undefined;
+  color?: string | undefined;
+  key?: string | undefined;
+  autoFocus?: boolean | undefined;
+  disabled?: boolean | undefined;
+  showDateDisplay?: boolean | undefined;
 };
 
 export const Reservas = () => {
@@ -18,47 +25,98 @@ export const Reservas = () => {
     {
       startDate: new Date(),
       endDate: undefined,
-      key: 'selection',
+      key: "selection",
     },
   ]);
+
+  console.log("ranges", ranges);
 
   const definedRanges: StaticRange[] = [
     {
       range: () => ({
         startDate: new Date(),
         endDate: new Date(),
-        key: 'selection',
+        key: "selection",
       }),
       isSelected: (range) => {
         const today = new Date();
-        return range.startDate?.getTime() === today.getTime() && range.endDate?.getTime() === today.getTime();
+        return (
+          range.startDate?.getTime() === today.getTime() &&
+          range.endDate?.getTime() === today.getTime()
+        );
       },
-      label: 'Hoy',
+      label: "Hoy",
     },
     {
       range: () => ({
         startDate: addDays(new Date(), 1),
         endDate: addDays(new Date(), 1),
-        key: 'selection',
+        key: "selection",
       }),
       isSelected: (range) => {
         const tomorrow = addDays(new Date(), 1);
-        return range.startDate?.getTime() === tomorrow.getTime() && range.endDate?.getTime() === tomorrow.getTime();
+        return (
+          range.startDate?.getTime() === tomorrow.getTime() &&
+          range.endDate?.getTime() === tomorrow.getTime()
+        );
       },
-      label: 'Mañana',
+      label: "Mañana",
     },
     // ... puedes agregar más rangos predefinidos si lo deseas
   ];
 
   return (
     <Layout>
-      <h1>Reservas</h1>
-      <DateRangePicker
-        ranges={ranges}
-        onChange={(item) => setRanges([item.selection])}
-        locale={es}
-        staticRanges={definedRanges}
-      />
+      <div style={{ textAlign: "center" }}>
+        <h3>Reservas</h3>
+        <DateRangePicker
+          ranges={ranges}
+          onChange={(item) => {
+            const newArr: Range[] = [...ranges];
+            if (Array.isArray(newArr) && newArr.length > 0) {
+              newArr.pop();
+            }
+            console.log("item", item);
+            newArr.push({
+              startDate: item.selection.startDate,
+              endDate: item.selection.endDate,
+              key: "selection",
+            });
+            setRanges(newArr);
+          }}
+          locale={es}
+          staticRanges={[]}
+          inputRanges={[]}
+          months={2} // Muestra dos meses
+          direction="horizontal" // Organiza los meses en dirección horizontal
+        />
+
+        <h3>Elige la cantidad de personas</h3>
+        <Row>
+          <Col md={4} sm={12}></Col>
+          <Col md={4} sm={12} style={{ textAlign: "justify" }}>
+            <div style={{ marginBottom: "10px" }}>
+              <Button>-</Button> Bebes (0) <Button>+</Button>
+            </div>
+            <div style={{ marginBottom: "10px" }}>
+              <Button>-</Button> Niños/Adolescentes (0) <Button>+</Button>
+            </div>
+            <div style={{ marginBottom: "10px" }}>
+              <Button>-</Button> Adultos (0) <Button>+</Button>
+            </div>
+          </Col>
+          <Col md={4} sm={12}></Col>
+        </Row>
+
+        <h3>Realiza el pago</h3>
+        <Row>
+          <Col md={4} sm={12}></Col>
+          <Col md={4} sm={12}>
+            <PaypalButtonDiv />
+          </Col>
+          <Col md={4} sm={12}></Col>
+        </Row>
+      </div>
     </Layout>
   );
 };
