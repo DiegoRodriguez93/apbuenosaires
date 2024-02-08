7ac4010cf1661e657c96f161f9559ee544026f61
unknown <sebastianrodriguezpaiva@gmail.com>
2024-02-08 14:35:49 -0300

    Fix underline

--- src/components/PropertyList/index.js
+++ src/components/PropertyList/index.js
@@ -11,15 +11,15 @@ const PropertyList = ({ properties, loading }) => {
     <div className={styles.propertyList}>
       {properties.map((property) => (
         property && property.id ? (
-          <Link href={`/property/${property.id}`} key={property.id} passHref>
+          <Link style={{ textDecoration: 'none' }} href={`/property/${property.id}`} key={property.id} passHref>
             <div className={styles.propertyCard}>
               <div
                 className={styles.propertyImage}
                 style={{ backgroundImage: `url(${property.images && property.images[0] ? property.images[0] : ''})` }}
               />
               <div className={styles.propertyInfo}>
-                <h3 className={styles.propertyTitle}>{property.title || 'No Title'}</h3>
-                <p className={styles.propertyPrice}>{property.price ? `${property.price} USD` : 'Price not available'}</p>
+                <h3 className={styles.propertyTitle} >{property.title || 'No Title'}</h3>
+                <p className={styles.propertyPrice} style={{ textDecoration: 'none' }}>{property.price ? `${property.price} USD Por noche` : 'Price not available'}</p>
                 <p className={styles.propertyDetails}>
                   {property.description 
                     ? (property.description.length > 100 
