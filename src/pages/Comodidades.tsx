c8be1f4a8bf476368023c9b6cd7260d51389be87
unknown <sebastianrodriguezpaiva@gmail.com>
2024-02-15 09:53:09 -0300

    updates

--- src/pages/Comodidades.tsx
+++ src/pages/Comodidades.tsx
@@ -13,7 +13,7 @@ todas las comodidades para que te sientas como en tu propia casa.<br/><br/>
 
 Los apartamentos los hemos pensado tanto para viajes de turismo, como de trabajo, de estudio o cualquier motivo que te acerque a esta maravillosa ciudad.<br/><br/>
 
-Contamos con 3 apartamentos en el barrio de Monserrat(pegadito a San Telmo) para hasta 6 personas y un apartamento en la zona de Palermo para hasta 4 personas.<br/><br/>
+Contamos con 3 apartamentos en el barrio de Montserrat(pegadito a San Telmo) para hasta 6 personas y un apartamento en la zona de Palermo para hasta 4 personas.<br/><br/>
 
 Te esperamos!
 <br/><br/></p>
